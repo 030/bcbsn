e89dd813155e5b40cdaa0b2bbed5959c00850521
Jorian van Oostenbrugge <jovofdt@gmail.com>
2019-05-06 11:35:46 +0200

    [GH-18] Refactor code so we can test the actual post body. Add additional error checking to make sure we get the expected response from BitBucket. Use a custom httpClient which has a timeout (so it doesn't wait indefinitely when the sever does not respond). Update params so they are aligned with how Bitbucket calls them, to avoid potential confusion.

--- main.go
+++ main.go
@@ -1,49 +1,58 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"strings"
 	"time"
-
-	"github.com/030/go-utils/utils"
-	"github.com/030/golang-bitbucket-cloud-oauth-dance/oauthdance"
-	log "github.com/sirupsen/logrus"
 )
 
 type Body struct {
-	State       string
-	Key         string
-	Name        string
-	URL         string
-	Description string
+	UUID  string `json:"uuid"`  // The Git commit hash
+	Key   string `json:"key"`   // The Build ID
+	URL   string `json:"url"`   // The URL of the specific build
+	State string `json:"state"` // The state of the build
+	Name  string `json:"name"`  // The identifier of the build
 }
 
-var netClient = http.Client{
+var httpClient = http.Client{
 	Timeout: time.Second * 10,
 }
 
 const bitbucketAPIEndpoint = "https://api.bitbucket.org/2.0/repositories"
 
-// We need to be able to overwrite the endpoint in our unit tests
-// So we use the approach as described here: https://stackoverflow.com/a/33774754/10224882
-func buildStatus(bearer, buildState, gitCommit, owner, repositoryOwner, buildNumber, buildURL string) error {
-	return buildStatusImpl(bitbucketAPIEndpoint, bearer, buildState, gitCommit, owner, repositoryOwner, buildNumber, buildURL)
+func prepareBody(commit, key, url, state, name string) ([]byte, error) {
+	b := Body{commit, key, url, state, name}
+	return json.Marshal(b)
 }
 
-func buildStatusImpl(bitbucketEndpoint, bearer, buildState, gitCommit, owner, repositoryOwner, buildNumber, buildURL string) error {
-	body := strings.NewReader(`{"key":"` + buildNumber + `","state":"` + buildState + `","url":"` + buildURL + `"}`)
-	req, err := http.NewRequest("POST", bitbucketEndpoint+"/"+owner+"/"+repositoryOwner+"/commit/"+gitCommit+"/statuses/build", body)
+// We need to be able to overwrite the endpoint in our unit tests
+// So we use the approach as described here: https://stackoverflow.com/a/33774754/10224882
+func buildStatus(owner, repoSlug, commit, key, url, state, name string) error {
+	return buildStatusImpl(httpClient, bitbucketAPIEndpoint, owner, repoSlug, commit, key, url, state, name)
+}
+
+func buildStatusImpl(httpClient http.Client, bitbucketEndpoint, owner, repoSlug, commit, key, url, state, name string) error {
+	body, err := prepareBody(commit, key, url, state, name)
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Authorization", "Bearer "+bearer)
+
+	req, err := http.NewRequest("POST", bitbucketEndpoint+"/"+owner+"/"+repoSlug+"/commit/"+commit+"/statuses/build", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := netClient.Do(req)
+	resp, err := httpClient.Do(req)
 	if resp != nil {
+		if resp.StatusCode != http.StatusCreated {
+			return fmt.Errorf("Expected %d, but got %s", http.StatusCreated, resp.Status)
+		}
 		defer resp.Body.Close()
 	}
 
@@ -58,17 +67,12 @@ func buildStatusImpl(bitbucketEndpoint, bearer, buildState, gitCommit, owner, re
 }
 
 func main() {
-	var keyString = flag.String(
-		"keyString",
-		"",
-		"The concatenate key and string, e.g. key:string")
-
-	var buildState = flag.String(
+	var state = flag.String(
 		"buildState",
 		"",
 		"The buildState, e.g. SUCCESSFUL, INPROGRESS or FAILED")
 
-	var gitCommit = flag.String(
+	var commit = flag.String(
 		"gitCommit",
 		"",
 		"The gitCommit, e.g. 57484fd5460017aef111e8b4ec116a30ff0b4904")
@@ -78,27 +82,28 @@ func main() {
 		"",
 		"The owner, e.g. your-name")
 
-	var repositoryName = flag.String(
+	var repoSlug = flag.String(
 		"repositoryName",
 		"",
 		"The repositoryName, e.g. some-repository")
 
-	var buildNumber = flag.String(
+	var key = flag.String(
 		"buildNumber",
 		"",
 		"The buildNumber, e.g. buildNumber")
 
-	var buildURL = flag.String(
+	var url = flag.String(
 		"buildURL",
 		"",
 		"The buildURL, e.g. buildURL")
 
-	utils.Debug()
+	var name = flag.String(
+		"name",
+		"",
+		"An identifier for the build")
 
-	accessToken, err := oauthdance.Bearer(*keyString)
+	err := buildStatus(*owner, *repoSlug, *commit, *key, *url, *state, *name)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	buildStatus(accessToken, *buildState, *gitCommit, *owner, *repositoryName, *buildNumber, *buildURL)
 }
